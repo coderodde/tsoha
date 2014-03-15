aaeef749351af1ce92dab596be6cd7b5f83f4a6c
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-03-15 15:24:01 +0200

    Simple DB-listing is now working.

--- multilog/src/java/ArticleServlet.java
+++ multilog/src/java/ArticleServlet.java
@@ -9,6 +9,8 @@ import org.postgresql.ds.PGPoolingDataSource;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -54,69 +56,16 @@ public class ArticleServlet extends HttpServlet {
     }
 
     protected String getArticlesFromDB() {
-//        PGPoolingDataSource source = new PGPoolingDataSource();
-//        source.setDataSourceName("PGDSfdsdfa");
-//        source.setServerName("users.cs.helsinki.fi");
-//        source.setDatabaseName("rodionef");
-//        source.setUser("rodionef");
-//        source.setPassword("ccac8eaa12c53175");
-//        source.setMaxConnections(100);
-//
-//        Connection conn = null;
-//
-//        try {
-//            conn = source.getConnection();
-//        } catch (SQLException e) {
-//            return "SQLException: " + e.getMessage();
-//        }
-//
-//        try {
-//            conn.close();
-//        } catch (SQLException e) {
-//            return "SQLException (close): " + e.getMessage();
-//        }
-//
-//        return "DB is working!";
-
-//        Context cxt = null;
-//        Context cxt2 = null;
-//        DataSource ds = null;
-//
-//        try {
-//            cxt = new InitialContext();
-//            cxt2 = (Context) cxt.lookup("java:/comp/env");
-//            ds = (DataSource) cxt2.lookup("jdbc/rodionef");
-////            ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/rodionef");
-//        } catch (NamingException ne) {
-//            return "NamingException: " + ne.getMessage();
-//        }
-//
-//        Connection conn = null;
-//
-//        try {
-//            conn = ds.getConnection();
-//        } catch (SQLException sqle) {
-//            return "SQLException: " + sqle.getMessage();
-//        }
-//
-//        try {
-//            conn.close();
-//        } catch (SQLException e) {
-//            return "SQLException (closing): " + e.getMessage();
-//        }
-//
-//        return "Database is here!";
-
         InitialContext ic = null;
         DataSource ds = null;
 
         try {
             ic = new InitialContext();
-            ds = (DataSource) ic.lookup("java:comp/env/jdbc/mlogDB");
+            ds = (DataSource) ic.lookup("java:/comp/env/jdbc/mlogDB");
         } catch (NamingException ne) {
             return "NamingException: " + ne.getMessage();
         }
-        
+
         Connection conn = null;
 
         try {
@@ -125,13 +74,48 @@ public class ArticleServlet extends HttpServlet {
             return "SQLException: " + sqle.getMessage();
         }
 
+        Statement statement = null;
+
         try {
-            conn.close();
-        } catch (SQLException e) {
-            return "SQLException (closing): " + e.getMessage();
+            statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM article;");
+
+
+
+            String resultText = getResults(rs);
+            statement.close();
+            return resultText;
+        } catch (SQLException sqle) {
+            try {
+                conn.close();
+            } catch (SQLException sqle2) {
+                // ignore.
+            }
+
+            return "SQLException: " + sqle.getMessage();
+        }
+    }
+
+    String getResults(ResultSet rs) {
+        StringBuilder sb = new StringBuilder(8192);
+        sb.append("<table>\n");
+
+        try {
+            while (rs.next()) {
+                sb.append("<tr><td>")
+                  .append(rs.getString(2))
+                  .append("</td><td>")
+                  .append(rs.getString(3))
+                  .append("</td><td>")
+                  .append(rs.getDate(4))
+                  .append("</td></tr>\n");
+            }
+        } catch (SQLException sqle) {
+            return "SQLException (beef): " + sqle.getMessage();
         }
 
-        return "Database is here!";
+        sb.append("</table>\n");
+        return sb.toString();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
