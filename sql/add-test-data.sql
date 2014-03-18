b61e14858a296104da3958130457d66415bfbc74
coderodde <rodion.efremov@helsinki.fi>
2014-03-18 17:54:39 +0200

    First user to DB.

---
+++ sql/add-test-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO users(username,
+                  first_name,
+                  last_name,
+                  email,
+                  description,
+                  user_type,
+                  created_at,
+                  updated_at)
+VALUES
+('rodde',
+ 'Rodion',
+ 'Efremov',
+ 'rodionef@cs.helsinki.fi',
+ 'Ima admin.',
+ 'Admin',
+ NOW(),
+ NOW());
