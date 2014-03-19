eee3d68924d072745c39cad8db05bec9c06323b7
Rodion Efremov <rodionef@svm-19.cs.helsinki.fi>
2014-03-19 17:21:03 +0200

    3 users added in add-sql...

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,4 +1,9 @@
+-- This SQL file loads sample content to DB.
+
+-- Creates users.
 INSERT INTO users(username,
+ 	          salt,
+	          passwd_hash,
                   first_name,
                   last_name,
                   email,
@@ -8,6 +13,8 @@ INSERT INTO users(username,
                   updated_at)
 VALUES
 ('rodde',
+ 'addksladfddsfkl5',
+ 'fdkslafjkdsljfkdl53',
  'Rodion',
  'Efremov',
  'rodionef@cs.helsinki.fi',
@@ -17,6 +24,59 @@ VALUES
  NOW());
 
 INSERT INTO users(username,
+                  salt,
+                  passwd_hash,
                   first_name,
                   last_name,
-                  email)
+                  email,
+	          show_email,
+                  description,
+                  user_type,
+                  created_at,
+		  updated_at)
+VALUES
+('modde',
+ 'fdsfd4r453',
+ 'fdkslfjdksl',
+ 'Woody',
+ 'Woodpecker',
+ 'woody@multilog.com',
+ FALSE,
+ 'Ima mod.',
+ 'Mod',
+ NOW(),
+ NOW());
+
+INSERT INTO users(username,
+                  salt,
+                  passwd_hash,
+                  first_name,
+                  last_name,
+                  email,
+	          show_email,
+                  description,
+                  user_type,
+                  created_at,
+		  updated_at)
+VALUES
+('User',
+ 'fdsf543d4r453',
+ 'fdkslfjfdsdksl',
+ 'Pekka',
+ 'Murkka',
+ 'murkkis@multilog.com',
+ FALSE,
+ 'Ima user.',
+ 'User',
+ NOW(),
+ NOW());
+
+-- Creates topics.
+
+
+
+-- Creates threads.
+
+
+
+-- Creates posts.
