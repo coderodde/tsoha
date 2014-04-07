4994f5c79b23db539c6c1e44ee8a8b5c714c5b4f
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-04-07 13:52:42 +0300

    Working again on SQL.

--- sql/drop-tables.sql
+++ sql/drop-tables.sql
@@ -1,6 +1,7 @@
 -- This SQL file drops all tables in multilog.
+
 -- Drops the tables.
-DROP TABLE IF EXISTS users, topics, threads, posts, message_reads;
+DROP TABLE IF EXISTS message_reads, posts, threads, topics, users;
 
 -- Drops everything else.
 DROP TYPE e_user_type;
