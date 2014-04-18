7d51dd11bbb49c73605c454e5cba0671ccb114d8
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-04-18 11:51:46 +0300

    Making post view pretty.

--- multilog/web/js/multilog.js
+++ multilog/web/js/multilog.js
@@ -4,12 +4,13 @@ function setReplyTarget(id, who) {
     }
 
     document.getElementById("hidden_input").value = id;
-    document.getElementById("reply_to").innerHTML = "Replying to " + who;
-    document.getElementById("dont_reply").innerHTML  = "Forget reply";
+    document.getElementById("reply_to").innerHTML = "Replying to " + who + ".";
+    document.getElementById("dont_reply").innerHTML  = "Forget reply!";
 }
 
 function forget() {
     document.getElementById("hidden_input").value = "";
     document.getElementById("reply_to").innerHTML = "";
     document.getElementById("dont_reply").innerHTML = "";
+    document.getElementById("dont_reply").style = "background-color: transparent;";
 }
