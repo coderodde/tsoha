14075579d4988ea045b17a89177495e69a0f2f3f
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-04-18 11:58:40 +0300

    Making it look pretty.

--- multilog/web/js/multilog.js
+++ multilog/web/js/multilog.js
@@ -12,5 +12,5 @@ function forget() {
     document.getElementById("hidden_input").value = "";
     document.getElementById("reply_to").innerHTML = "";
     document.getElementById("dont_reply").innerHTML = "";
-    document.getElementById("dont_reply").style = "background-color: transparent;";
+    document.getElementById("dont_reply").style.color = "#ff3300";
 }
